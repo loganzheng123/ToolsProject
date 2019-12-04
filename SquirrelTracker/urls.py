68c1421f252be4587e7e51461ad457b871846006
Longgang Zheng <lz2717@columbia.edu>
2019-12-04 19:33:44 +0000

    Create map page.

--- SquirrelTracker/urls.py
+++ SquirrelTracker/urls.py
@@ -3,5 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ex: /SquirrelTracker/
     path('', views.index, name='index'),
+    #ex: /SquirrelTracker/sightings/
+    path('sightings/', views.sightings, name='sightings'),
+    #ex: /SquirrelTracker/sightings/map
+    path('sightings/map', views.map, name='map'),
+    #ex: /SquirrelTracker/sightings/5/
+    path('sightings/<int:squirrel_census_id>/', views.update, name='update'),
+    #ex: /SquirrelTracker/sightings/add/
+    path('sightings/add/', views.add_sightings, name='add_sightings'),
+    #ex: /SquirrelTracker/sightings/stats/
+    path('sightings/stats/', views.stats, name='stats'),
 ]
--- SquirrelTracker/views.py
+++ SquirrelTracker/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Squirrel_Census
+
 def index(request):
     return HttpResponse("Hello, I am working on the Squirrel Tracker project.")
+
+def map(request):
+    sightings = Squirrel_Census.objects.all()
+    context = {'sightings':sightings}
+    return render(request,'SquirrelTracker/map.html',context)
+
+def sightings(request):
+    return HttpResponse("This page is reserved for the sightings list page.")
+
+def update(request,squirrel_census_id):
+    response = "This page is reserved for the update and delete of the %s sightings."
+    return HttpResponse(response % squirrel_census_id)
+
+def add_sightings(request):
+    return HttpResponse("This page is reserved for adding sightings.")
+
+def stats(request):
+    return HttpResponse("This page is reserved for presenting stats.")
