00097d1da0597bf7e9cc437cf8f4d893622ae29d
Longgang Zheng <lz2717@columbia.edu>
2019-12-05 18:12:40 +0000

    dd

--- SquirrelTracker/views.py
+++ SquirrelTracker/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from .models import Squirrel_Census
+from .forms import SightingForm
 
 def index(request):
     return HttpResponse("Hello, I am working on the Squirrel Tracker project.")
@@ -15,11 +17,36 @@ def sightings(request):
     return HttpResponse("This page is reserved for the sightings list page.")
 
 def update(request,squirrel_census_id):
-    response = "This page is reserved for the update and delete of the %s sightings."
-    return HttpResponse(response % squirrel_census_id)
+    sighting = Squirrel_Census.objects.get(id=squirrel_census_id)
+    if request.method == 'POST':
+        form = SightingForm(request.POST,instance=sighting)
+        if form.is_valid():
+            form.save()
+            return redirect(f'sightings/{squirrel_census_id}')
+    else:
+        form = SightingForm(instance=sighting)
+
+    context = {
+            'form': form,
+    }
+
+    return render(request, 'SquirrelTracker/update.html', context)
 
 def add_sightings(request):
-    return HttpResponse("This page is reserved for adding sightings.")
+    if request.method == 'POST':
+        form = SightingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/')
+    else:
+        form = SightingForm()
+
+    context = {
+            'form' : form,
+            'jazz' : True,
+    }
+
+    return render(request, 'SquirrelTracker/update.html', context)
 
 def stats(request):
     return HttpResponse("This page is reserved for presenting stats.")
